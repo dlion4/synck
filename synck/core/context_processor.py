46cec1c03b8ccd97bdafcff0e13a6f6ea7271ced
Jeckonia Kwasa <inctifra@gmail.com>
2024-05-01 10:46:26 +0300

    address update

--- synck/core/context_processor.py
+++ synck/core/context_processor.py
@@ -2,4 +2,4 @@ from .form import ContactForm
 
 
 def core_context_processor(request):
-    return dict(form=ContactForm())
+    return dict(form=ContactForm(), address="00100,Lithuli Avenue Nairobi, Kenya")
